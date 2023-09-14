51fcffa4e4b90e35c7694b481d8afdf43039c2fb
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2023-09-14 16:42:36 -0400

    lecture is finished

--- google.py
+++ google.py
@@ -4,3 +4,11 @@ input:['A','B','C','C']
 output:['A','C','B','C']
 
 """
+input:['A','B','C','C']
+
+def google_move(input):
+    for s in input:
+        if s[0] == s[1]:
+            s[1] == s[3]
+
+print(google_move(input))
